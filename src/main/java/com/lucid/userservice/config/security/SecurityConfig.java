535544565027db859281bc730ccb108e37a3b179
KMK_HM <kyun9151@gmail.com>
2024-03-19 18:11:59 +0900

    feat: security 기본 설정 및 redis 설정

--- src/main/java/com/lucid/userservice/config/security/SecurityConfig.java
+++ src/main/java/com/lucid/userservice/config/security/SecurityConfig.java
@@ -1,7 +1,22 @@
 package com.lucid.userservice.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+                .headers((e) -> e.frameOptions((a) -> a.sameOrigin()));
+
+        return http.build();
+    }
 }
--- src/main/java/com/lucid/userservice/controller/MemberController.java
+++ src/main/java/com/lucid/userservice/controller/MemberController.java
@@ -2,10 +2,19 @@ package com.lucid.userservice.controller;
 
 import com.lucid.userservice.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/")
 public class MemberController {
+
     private final MemberService memberService;
+
+    @GetMapping("/home")
+    public String home() {
+        return "hi";
+    }
 }
